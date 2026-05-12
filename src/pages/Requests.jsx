83db3316953032e9618c38eafe1b897a582fe566
Brian mutai <bkmutai68w@gmail.com>
2026-05-12 13:03:10 +0300

    Request has been made

---
+++ src/pages/Requests.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import RequestCard from "../components/RequestCard";
+
+export default function Requests() {
+  const [requests, setRequests] = useState([
+    {
+      id: 1,
+      sender: "Brian Mutai",
+      skill: "React Basics",
+      status: "pending",
+    },
+    {
+      id: 2,
+      sender: "Densi Nela",
+      skill: "UI Design",
+      status: "pending",
+    },
+  ]);
+
+  // ACCEPT REQUEST
+  const acceptRequest = (id) => {
+    const updatedRequests = requests.map((request) =>
+      request.id === id
+        ? { ...request, status: "accepted" }
+        : request
+    );
+
+    setRequests(updatedRequests);
+  };
+
+  // REJECT REQUEST
+  const rejectRequest = (id) => {
+    const updatedRequests = requests.map((request) =>
+      request.id === id
+        ? { ...request, status: "rejected" }
+        : request
+    );
+
+    setRequests(updatedRequests);
+  };
+
+  return (
+    <div className="min-h-screen p-6 bg-gray-100">
+      <h1 className="text-2xl font-bold mb-6">
+        Skill Swap Requests
+      </h1>
+
+      <div className="grid gap-4">
+        {requests.map((req) => (
+          <RequestCard
+            key={req.id}
+            request={req}
+            onAccept={acceptRequest}
+            onReject={rejectRequest}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
